785a5a66d3d6f2d10eeb9f02e62b617df307bc05
te2ry <pl9429@naver.com>
2022-12-16 12:40:17 +0900

    Initial commit

---
+++ src/main/java/com/flow/flowassignment/FlowAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.flow.flowassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flow/flowassignment/FlowAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.flowassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
