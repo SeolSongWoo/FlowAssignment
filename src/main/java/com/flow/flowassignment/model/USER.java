4e4db75c1f973382efb24586a364bd37513ece26
te2ry <pl9429@naver.com>
2022-12-27 11:15:37 +0900

    FIX IndexOutOfBoundsException오류 수정.

--- src/main/java/com/flow/flowassignment/model/USER.java
+++ src/main/java/com/flow/flowassignment/model/USER.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class USER implements UserDetails {
     private int user_seq;
