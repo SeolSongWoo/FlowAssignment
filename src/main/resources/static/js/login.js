c39fab4030490aa9a5330e422d9781443544be15
te2ry <pl9429@naver.com>
2022-12-28 10:21:47 +0900

    FIX IndexOutOfBoundsException오류 수정.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,12 +1,28 @@
 function logincheck() {
     const id = $('#username').val();
     const password = $('#password').val();
-
+    const alphaDigit= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     if(id == ""  || password == "") {
         alert("아이디 또는 비밀번호를 입력해주세요.");
         return;
     }
-
+    if (id.indexOf(" ") >= 0) {
+        alert("아이디에는 공백이 들어갈 수 없습니다.");
+        id.focus();
+        return;
+    }
+    for (let i=0; i<id.length; i++) {
+        if (alphaDigit.indexOf(id.substring(i, i+1)) == -1) {
+            alert("아이디는 영문과 숫자의 조합만 사용할 수 있습니다.");
+            id.focus();
+            return;
+        }
+    }
+    if (password.indexOf(" ") >= 0) {
+        alert("비밀번호에는 공백이 들어갈 수 없습니다.");
+        password.focus();
+        return;
+    }
     const account = {
         'id' : id,
         'password' : password,
